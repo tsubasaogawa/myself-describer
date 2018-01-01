11dd71b2e0db49f390f043a7f197862443826939
Tsubasa Ogawa <hurikake@gmail.com>
2018-01-01 07:23:35 +0000

    change title

--- myself.py
+++ myself.py
@@ -5,6 +5,8 @@ from text_generator import TextGenerator
 
 app = Flask(__name__)
 model_file = './myself.model'
+title = 'virtual-ogawa'
+host = '0.0.0.0'
 
 @app.route('/', methods=['POST', 'GET'])
 def describe():
@@ -17,7 +19,7 @@ def describe():
     if generated_text is None:
       generated_text = "Sorry! '%s' is unknown word..." % start_word
 
-  return render_template('describe.html', title='myself describer', generated_text=generated_text)
+  return render_template('describe.html', title=title, generated_text=generated_text)
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', debug=True)
+  app.run(host=host, debug=True)
