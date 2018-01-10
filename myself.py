bdc7b311826e55accc0b06849e0361dd62dddf25
Tsubasa Ogawa <hurikake@gmail.com>
2018-01-10 14:17:01 +0000

    make it possible to use wtforms

--- myself.py
+++ myself.py
@@ -2,7 +2,8 @@
 
 from flask import Flask, render_template, request
 from text_generator import TextGenerator
-import TextForm
+from text_form import TextForm
+import sys
 
 app = Flask(__name__)
 model_file = './myself.model'
@@ -11,12 +12,9 @@ host = '0.0.0.0'
 
 @app.route('/', methods=['POST', 'GET'])
 def describe():
-  form = TextForm(request.form)
-  if not form.validate():
-    sys.exit(1)
-
   generated_text = ''
-  if request.method == 'POST':
+  form = TextForm(request.form)
+  if request.method == 'POST' and form.validate():
     start_word = form.start_word.data
     word_num = form.word_num.data
     generator = TextGenerator(model_file, start_word, word_num)
@@ -24,7 +22,7 @@ def describe():
     if generated_text is None:
       generated_text = "Sorry! '%s' is unknown word..." % start_word
 
-  return render_template('describe.html', title=title, generated_text=generated_text)
+  return render_template('describe.html', title=title, generated_text=generated_text, form=form)
 
 if __name__ == "__main__":
   app.run(host=host, debug=True)
