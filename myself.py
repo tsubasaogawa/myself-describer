1855847721dfb14d25050d67c72cdedf6d126760
Tsubasa Ogawa <hurikake@gmail.com>
2019-09-14 13:39:03 +0000

    fix lint warnings

--- myself.py
+++ myself.py
@@ -1,80 +1,106 @@
-# coding: utf-8
-
+"""
+Describe myself module.
+"""
+import os
+import re
+import glob
+from datetime import date
+import requests
 from flask import Flask, render_template, request
+
 from text_generator import TextGenerator
 from text_form import TextForm
-import sys, os
-import re, glob
-import requests
-from datetime import date
 
 
-app = Flask(__name__)
-title = 'virtual-ogawa'
-host = '0.0.0.0'
+APP = Flask(__name__)
+TITLE = 'virtual-ogawa'
+HOST = '0.0.0.0'
 
-model_path = '{}/model'.format(
-  os.path.dirname(os.path.abspath(__file__))
+MODEL_PATH = '{0}/model'.format(
+    os.path.dirname(os.path.abspath(__file__))
 )
-model_file_glob = '{}/model_iter_*'.format(model_path)
-model_file_url = os.environ.get('MODEL_FILE_URL')
-
-def download_model_file(url):
-  response = requests.get(url)
-  downloaded_path =  '{0}/{1}'.format(model_path, os.path.basename(model_file_url))
-  with open(downloaded_path, 'wb') as f:
-    f.write(response.content)
-
-  print('model downloaded')
-  return downloaded_path
-
-def get_latest_model_file():
-  model_files = glob.glob(model_file_glob)
-  if not len(model_files):
-    file_path = download_model_file(model_file_url)
-    model_files = [file_path]
-    # raise FileNotFoundError('no model file found')
-
-  return model_files[-1]
-
-def get_model_information():
-  latest_model_file = get_latest_model_file()
-  iteration = re.search(r'model_iter_(\d+)', latest_model_file)
-
-  modified = date.fromtimestamp(os.path.getmtime(latest_model_file)).strftime('%Y-%m-%d')
-  size = os.path.getsize(latest_model_file)
-  return {
-    'name': latest_model_file,
-    'iter': iteration.group(1),
-    'modified': modified,
-    'size': size
-  }
+MODEL_PATH_GLOB = '{0}/model_iter_*'.format(MODEL_PATH)
+MODEL_FILE_REGEX = r'model_iter_(\d+)'
 
 # ------------------------------ #
 
-@app.route('/', methods=['POST', 'GET'])
+def download_model_file(url):
+    """
+    Download model file from internet.
+    Environment variable `MODEL_FILE_URL` is must be set.
+    """
+    response = requests.get(url)
+    downloaded_path = '{0}/{1}'.format(
+        MODEL_PATH,
+        os.path.basename(url),
+    )
+    with open(downloaded_path, 'wb') as handler:
+        handler.write(response.content)
+
+    print('model downloaded')
+    return downloaded_path
+
+def get_latest_model_file_name():
+    """
+    Get model file name in local storage.
+    """
+    local_model_files = glob.glob(MODEL_PATH_GLOB)
+    if not local_model_files:
+        model_url = os.environ.get('MODEL_FILE_URL')
+        file_path = download_model_file(model_url)
+        local_model_files = [file_path]
+
+    return local_model_files[-1]
+
+def get_model_information():
+    """
+    Get information of the model file.
+    """
+    model_filename = get_latest_model_file_name()
+    iteration = re.search(MODEL_FILE_REGEX, model_filename)
+
+    modified = date.fromtimestamp(
+        os.path.getmtime(model_filename)
+    ).strftime('%Y-%m-%d')
+    size = os.path.getsize(model_filename)
+
+    return {
+        'name': model_filename,
+        'iter': iteration.group(1),
+        'modified': modified,
+        'size': size,
+    }
+
+# ------------------------------ #
+
+@APP.route('/', methods=['POST', 'GET'])
 def describe():
-  generated_text = ''
-  model = None
-  try:
-    model = get_model_information()
-  except FileNotFoundError as e:
-    generated_text = e
+    """
+    Describe myself.
+    """
+    generated_text = ''
+    model = None
+    try:
+        model = get_model_information()
+    except FileNotFoundError as err:
+        generated_text = err
 
-  form = TextForm(request.form)
-  if model and request.method == 'POST' and form.validate():
-    start_word = form.start_word.data
-    word_num = form.word_num.data
-    generator = TextGenerator(model['name'], start_word, word_num)
-    generated_text = generator.execute()
-    if generated_text is None:
-      generated_text = "Sorry! '%s' is unknown word..." % start_word
+    form = TextForm(request.form)
+    if model and request.method == 'POST' and form.validate():
+        start_word = form.start_word.data
+        word_num = form.word_num.data
+        generator = TextGenerator(model['name'], start_word, word_num)
+        generated_text = generator.execute()
+        if generated_text is None:
+            generated_text = "Sorry! '{0}' is unknown word...".format(start_word)
 
-  return render_template('describe.html',
-      title=title,
-      generated_text=generated_text,
-      form=form,
-      model=model)
+    return render_template(
+        'describe.html',
+        title=TITLE,
+        generated_text=generated_text,
+        form=form,
+        model=model,
+    )
 
 if __name__ == "__main__":
-  app.run(host=host, debug=True)
+    APP.run(host=HOST, debug=True)
