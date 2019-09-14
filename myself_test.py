5b0341f5cfec23345a08ddaef37e1fc5272d7333
Tsubasa Ogawa <hurikake@gmail.com>
2019-09-14 14:36:56 +0000

    add test and circleci

---
+++ myself_test.py
@@ -0,0 +1,14 @@
+import unittest
+import myself
+
+
+class TestMyself(unittest.TestCase):
+    def test_download_model_file(self):
+        pass
+
+    def test_get_latest_model_file_name(self):
+        pass
+
+    def test_get_model_information(self):
+        pass
+
